e2758f56653649c7368d0a3ecde76bb3cf1bb6e2
UPPARAPALLI SURYA SASIKANTH <ussk2284@gmail.com>
2025-12-06 20:03:10 +0530

    Update Smallest in Array.py

--- ProblemsOnArrays/Smallest in Array.py
+++ ProblemsOnArrays/Smallest in Array.py
@@ -5,3 +5,14 @@ print("Smallest element is:",a[0])
 Output:
 Smallest element is:2
 '''
+
+
+numbers = [5, 2, 8, 1, 7]
+if not numbers:
+    smallest_number = None # Handle empty list
+else:
+    smallest_number = numbers[0]
+    for num in numbers:
+        if num < smallest_number:
+            smallest_number = num
+print(smallest_number) # Output: 1
